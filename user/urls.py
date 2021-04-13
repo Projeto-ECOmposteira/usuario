3240844bc2412698f67e5557f4b06dbbea27d938
Leonardo Medeiros <leonardomedeiros.6@gmail.com>
2021-04-12 21:18:08 -0300

    Add registration

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from user.views import CustomObtainTokenPairView
+from user.views import CustomObtainTokenPairView, RegisterView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 
 urlpatterns = [
     path('login/', CustomObtainTokenPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', RegisterView.as_view(), name='auth_register'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.models import User
+
+from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-from .serializers import CustomTokenObtainPairSerializer
+from .serializers import CustomTokenObtainPairSerializer, RegisterSerializer
 
 
 class UserView(APIView):
@@ -16,3 +19,8 @@ class UserView(APIView):
 class CustomObtainTokenPairView(TokenObtainPairView):
     permission_classes = (AllowAny,)
     serializer_class = CustomTokenObtainPairSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
