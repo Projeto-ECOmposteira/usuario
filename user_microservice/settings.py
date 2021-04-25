c8c4a96ec3b39cd2fca77b07f2ede794b8320044
Andre Pinto <andrelucax@gmail.com>
2021-04-25 19:21:59 -0300

    Add config for production email

--- user_microservice/settings.py
+++ user_microservice/settings.py
@@ -137,4 +137,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+if (ENVIRONMENT == 'PRODUCTION'):
+    # Email settings
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = config('EMAIL_HOST')
+    EMAIL_PORT = config('EMAIL_PORT')
+    EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+    EMAIL_USE_TLS = True
+    EMAIL_USE_SSL = False
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
